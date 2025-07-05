e97a4a3100dfc09f71ce984b308e3a084bc229d0
asuma312 <39178303+asuma312@users.noreply.github.com>
2025-07-05 16:28:34 -0300

    changed routine from thread to async

--- app/crawlers/steam.py
+++ app/crawlers/steam.py
@@ -1,8 +1,9 @@
-import requests
+import aiohttp
 from dotenv import load_dotenv
 import os
 from app.models.crawlers import AppIdResponse, AppDetailResponse
 from app.utils.logger import logger
+import asyncio
 load_dotenv()
 
 
@@ -20,40 +21,38 @@ class Crawler:
         "app_detail":"https://store.steampowered.com/api/appdetails"
     }
 
-    def __init__(self):
-        self.session: requests.Session() = requests.Session()
-        self.session.proxies = self.proxies
+
+    async def detect_ip(self):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            async with session.get('https://api.ipify.org?format=json', proxy=self.proxies.get('https')) as response:
+                print(f"Response: {await response.json()}")
 
 
-    def get_app_detail(self, app_id):
+    async def get_app_detail(self, app_id):
         params = {
             "appids": app_id,
         }
-        response = self.session.get(self.urls["app_detail"], params=params)
-        response.raise_for_status()
-        print(response.json())
-        data = response.json()[str(app_id)]
-        if not data['success']:
-            logger.error(f"Failed to retrieve app details for app ID {app_id}.")
-            return None
-        response = AppDetailResponse.from_dict(data)
-        return response
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), proxy=self.proxies.get('https')) as session:
+            async with session.get(self.urls["app_detail"], params=params) as response:
+                response.raise_for_status()
+                data = await response.json()
+                app_data = data.get(str(app_id))
+                if not app_data or not app_data.get('success'):
+                    logger.error(f"Failed to retrieve app details for app ID {app_id}.")
+                    return None
+                response = AppDetailResponse.from_dict(app_data)
+                return response
 
 
 
-    def get_app_ids(self)-> AppIdResponse:
+    async def get_app_ids(self)-> AppIdResponse:
         params = {
             "format": "json"
         }
-        response = self.session.get(self.urls["app_list"], params=params)
-        response.raise_for_status()
-        data = response.json()
-        response = AppIdResponse.from_dict(data)
-        logger.info(f"Total apps found: {len(response.applist.apps)}")
-        return response
-
-if __name__ == "__main__":
-    crawler = Crawler()
-    app_id = 2990
-    r = crawler.get_app_detail(app_id)
-    print(r)
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), proxy=self.proxies.get('https')) as session:
+            async with session.get(self.urls["app_list"], params=params) as response:
+                response.raise_for_status()
+                data = await response.json()
+                response = AppIdResponse.from_dict(data)
+                logger.info(f"Total apps found: {len(response.applist.apps)}")
+                return response
