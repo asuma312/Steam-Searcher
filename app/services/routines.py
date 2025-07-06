50322f25852b62e35ce2e323adca8625b348b6bd
asuma312 <39178303+asuma312@users.noreply.github.com>
2025-07-06 08:58:22 -0300

    changed from native duckdb to parquet
    Duckdb keeps stuttering when multiple inserts are done at the same time, going for parquet files helps to keep both the threads and async working

--- app/services/routines.py
+++ app/services/routines.py
@@ -1,59 +1,70 @@
 import asyncio
 import threading
 from typing import List, Coroutine
-from app.db.setup import get_db
-from app.models.sql import AppDetail, AppId
+import pandas as pd
+
+import uuid
 from app.models.crawlers import AppIdResponse as CrawlerAppIdResponse, AppDetailResponse as CrawlerAppDetailResponse, \
     AppDetail as AppDetailModel
 from app.crawlers.steam import Crawler
 from app.utils.logger import logger
-from sqlalchemy import text
-from sqlalchemy.orm import Session
 from tqdm import tqdm
+import os
+
+db_path = r"C:\Users\Lucas\Documents\GitHub\Steam-Searcher\app\db\db_files\parquet_files"
+db_path = os.path.join(
+    os.path.dirname(__file__),
+    '..', 'db', 'db_files', 'parquet_files'
+)
+ids_path = os.path.join(db_path, 'ids')
+details_path = os.path.join(db_path, 'details')
+
+os.makedirs(ids_path, exist_ok=True)
+os.makedirs(details_path, exist_ok=True)
 
 
 async def update_app_id():
     logger.info("Starting app ID update...")
-    session: Session = next(get_db())
     crawler = Crawler()
     try:
         app_ids_response: CrawlerAppIdResponse = await crawler.get_app_ids()
-        id_objects = [
-            AppId(app_id=item.appid, app_name=item.name)
+        id_list = [
+            {'app_id': item.appid, 'app_name': item.name}
             for item in app_ids_response.applist.apps
         ]
-        if not id_objects:
+        if not id_list:
             logger.info("No new app IDs found from the crawler.")
             return
-        session.add_all(id_objects)
-        session.commit()
-        logger.info(f"Batch inserted/updated {len(id_objects)} app IDs.")
+
+        df = pd.DataFrame(id_list)
+        output_file = os.path.join(ids_path, 'app_ids.parquet')
+        df.to_parquet(output_file, index=False)
+        logger.info(f"Saved {len(id_list)} app IDs to {output_file}.")
     except Exception as e:
         logger.error(f"Failed to update app IDs: {e}")
-        session.rollback()
-    finally:
-        session.close()
 
 
-def do_batch_insert(details_to_add: List[AppDetail]):
+def do_batch_insert(details_to_add: List[AppDetailModel]):
     if not details_to_add:
         logger.info("No app details generated in this thread's batch.")
         return
 
-    session: Session = next(get_db())
     try:
-        session.add_all(details_to_add)
-        session.commit()
-        logger.info(f"Thread {threading.get_ident()} successfully batch inserted {len(details_to_add)} app details.")
+        data_dicts = [detail.model_dump() for detail in details_to_add]
+        df = pd.DataFrame(data_dicts)
+
+        filename = f"{uuid.uuid4()}.parquet"
+        output_file = os.path.join(details_path, filename)
+
+        df.to_parquet(output_file, index=False)
+        logger.info(
+            f"Thread {threading.get_ident()} successfully saved {len(details_to_add)} app details to {filename}.")
     except Exception as e:
-        logger.error(f"Thread {threading.get_ident()} database batch insert failed: {e}")
-        session.rollback()
-    finally:
-        session.close()
+        logger.error(f"Thread {threading.get_ident()} parquet file write failed: {e}")
 
 
 async def fetch_and_prepare_detail(app_id: int, crawler: Crawler, semaphore: asyncio.Semaphore,
-                                   max_tries=5) -> AppDetail:
+                                   max_tries=5) -> AppDetailModel:
     async with semaphore:
         app_detail_response = None
         for attempt in range(max_tries):
@@ -70,10 +81,43 @@ async def fetch_and_prepare_detail(app_id: int, crawler: Crawler, semaphore: asy
 
         if not app_detail_response or not app_detail_response.data:
             logger.warning(f"No valid data for App ID {app_id}, creating placeholder.")
-            return AppDetail(steam_appid=app_id, name="N/A")
+            return AppDetailModel(
+                type='N/A',
+                name='Unknown',
+                steam_appid=app_id,
+                required_age=0,
+                is_free=False,
+                dlc=[],
+                detailed_description='',
+                about_the_game='',
+                short_description='',
+                supported_languages='',
+                header_image='',
+                website='',
+                pc_requirements={},
+                mac_requirements={},
+                linux_requirements={},
+                developers=[],
+                publishers=[],
+                price_overview={},
+                packages=[],
+                platforms={},
+                metacritic={},
+                categories=[],
+                genres=[],
+                screenshots=[],
+                movies=[],
+                recommendations={},
+                achievements={},
+                release_date={},
+                support_info={},
+                background='',
+                content_descriptors={},
+                tags=[]
+            )
 
-        app_detail_model: AppDetailModel = app_detail_response.data
-        return AppDetail(**app_detail_model.dict())
+
+        return app_detail_response.data
 
 
 async def generate_details_for_chunk(app_ids_chunk: List[int], progress_bar: tqdm, async_concurrency: int):
@@ -82,14 +126,14 @@ async def generate_details_for_chunk(app_ids_chunk: List[int], progress_bar: tqd
 
     tasks: List[Coroutine] = [fetch_and_prepare_detail(app_id, crawler, semaphore) for app_id in app_ids_chunk]
 
-    results: List[AppDetail] = []
+    results: List[AppDetailModel] = []
     for future in asyncio.as_completed(tasks):
         result = await future
-        results.append(result)
+        if result:
+            results.append(result)
         if progress_bar:
             progress_bar.update(1)
 
-    await crawler.close_session()
     return results
 
 
@@ -101,50 +145,66 @@ def thread_worker(app_ids_chunk: List[int], progress_bar: tqdm, async_concurrenc
     do_batch_insert(details_list)
 
 
-async def update_app_id_details(num_threads=5, batch_size=200, async_concurrency_per_thread=10):
-    query_session = next(get_db())
-    try:
-        app_ids_query = text(
-            f"""
-            SELECT app_id FROM app_id
-            WHERE app_id NOT IN (SELECT steam_appid FROM app_details)
-            LIMIT {batch_size}
-            """
-        )
-        app_ids: List[int] = query_session.execute(app_ids_query).scalars().all()
-    finally:
-        query_session.close()
-
-    total_ids = len(app_ids)
-    if total_ids == 0:
-        logger.info("No app details to update. Database is up-to-date.")
+async def update_app_id_details(num_threads=5, batch_size=200, async_concurrency_per_thread=10) -> bool:
+    master_id_file = os.path.join(ids_path, 'app_ids.parquet')
+    if not os.path.exists(master_id_file):
+        logger.error("Master app_ids.parquet file not found. Please run update_app_id first.")
         return True
 
-    progress = tqdm(total=total_ids, desc="Atualizando detalhes dos aplicativos", ncols=100)
+    try:
+        all_ids_df = pd.read_parquet(master_id_file, columns=['app_id'])
+        all_ids = set(all_ids_df['app_id'].unique())
 
-    chunks = [app_ids[i::num_threads] for i in range(num_threads)]
+        processed_ids = set()
+        for file in os.listdir(details_path):
+            file_path = os.path.join(details_path, file)
+            if os.path.getsize(file_path) == 0:
+                logger.error(f"File {file} has no content. Deleting.")
+                os.remove(file_path)
+        if os.path.exists(details_path) and len(os.listdir(details_path)) > 0:
+            processed_df = pd.read_parquet(details_path, columns=['steam_appid'])
+            processed_ids = set(processed_df['steam_appid'].unique())
 
-    threads: List[threading.Thread] = []
-    for chunk in chunks:
-        if chunk:
-            thread = threading.Thread(
-                target=thread_worker,
-                args=(chunk, progress, async_concurrency_per_thread)
-            )
-            threads.append(thread)
-            thread.start()
+        app_ids_to_fetch = list(all_ids - processed_ids)
 
-    for thread in threads:
-        thread.join()
+        total_ids = len(app_ids_to_fetch)
+        if total_ids == 0:
+            logger.info("No new app details to update. All details are up-to-date.")
+            return True
 
-    progress.close()
-    return False
+        logger.info(f"Found {total_ids} app IDs needing details. Processing a batch of {min(total_ids, batch_size)}.")
+
+        app_ids_batch = app_ids_to_fetch[:batch_size]
+
+        progress = tqdm(total=len(app_ids_batch), desc="Atualizando detalhes dos aplicativos", ncols=100)
+
+        chunks = [app_ids_batch[i::num_threads] for i in range(num_threads)]
+
+        threads: List[threading.Thread] = []
+        for chunk in chunks:
+            if chunk:
+                thread = threading.Thread(
+                    target=thread_worker,
+                    args=(chunk, progress, async_concurrency_per_thread)
+                )
+                threads.append(thread)
+                thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        progress.close()
+        return False
+
+    except Exception as e:
+        logger.error(f"An error occurred during app detail update: {e}")
+        return True
 
 
 async def main():
     await update_app_id()
     while True:
-        is_done = await update_app_id_details(num_threads=5, batch_size=500, async_concurrency_per_thread=10)
+        is_done = await update_app_id_details(num_threads=6, batch_size=300, async_concurrency_per_thread=6)
         if is_done:
             break
         logger.info("Batch processed. Checking for more app details to update...")
