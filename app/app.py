fb464ba1f802c2ba13aa86f8911b7922d0527bfa
asuma312 <39178303+asuma312@users.noreply.github.com>
2025-10-23 17:00:23 -0300

    fix duckdb bug

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import *
-
+from sqlalchemy import text
 
 
 
@@ -10,12 +10,13 @@ def create_app():
     from app.db import db_path
     import duckdb
     CORS(app)
-    from app.db.setup import engine, Base
+    from app.db.setup import engine, Base, get_db
     from app.models.sql import AppDetail, AppId
     Base.metadata.create_all(bind=engine)
-    with duckdb.connect(db_path) as conn:
+
+    with engine.connect() as conn:
         # duckdb doesnt have materialized views
-        conn.execute(
+        conn.execute(text(
             """
             CREATE VIEW IF NOT EXISTS app_genres AS
             SELECT DISTINCT element AS genre
@@ -23,14 +24,16 @@ def create_app():
                  UNNEST(genres) AS element
             """
         )
+        )
 
-        conn.execute(
+        conn.execute(text(
             """
             CREATE VIEW IF NOT EXISTS app_categories AS
             SELECT DISTINCT element AS category
             FROM detail,
                  UNNEST(categories) AS element
-            """
+            """)
         )
+    engine.dispose()
     app.register_blueprint(api_bp)
     return app
